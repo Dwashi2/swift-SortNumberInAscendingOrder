b46503be62f5d13d3b81dce3408e2ab489d89de2
Daniel Washington <danielvertigo@hotmail.com>
2021-05-27 10:52:05 -0300

    Files

--- SortNumbersInAscendingOrder/ViewController.swift
+++ SortNumbersInAscendingOrder/ViewController.swift
@@ -5,6 +5,26 @@
 //  Created by Daniel Washington Ignacio on 27/05/21.
 //
 
+
+/*
+ Create a function that takes an array of numbers and returns a new array, sorted in ascending order (smallest to biggest).
+
+ Rules
+
+ Sort numbers array in ascending order.
+ If functions argument is nil or an empty array, return an empty array.
+ Return new array of sorted numbers.
+ 
+ Examples
+ sortNumsAscending([1, 2, 10, 50, 5]) ➞ [1, 2, 5, 10, 50]
+
+ sortNumsAscending([80, 29, 4, -95, -24, 85]) ➞ [-95, -24, 4, 29, 80, 85]
+
+ sortNumsAscending(nil) ➞ []
+
+ sortNumsAscending([]) ➞ []
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +32,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(sortNumsAscending([1, 2, 10, 50, 5]))
+        print(sortNumsAscending([80, 29, 4, -95, -24, 85]))
+        print(sortNumsAscending(nil))
+        print(sortNumsAscending([]))
     }
 
+    func sortNumsAscending(_ arr: [Int]?) -> [Int] {
+        return arr?.sorted() ?? []
+    }
 
 }
 
