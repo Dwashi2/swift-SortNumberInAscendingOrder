86b87dcc494ba7996fa76f6e660dc6068603ed8d
Daniel Washington <danielvertigo@hotmail.com>
2021-05-27 10:48:08 -0300

    Initial Commit

---
+++ SortNumbersInAscendingOrder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SortNumbersInAscendingOrder
+//
+//  Created by Daniel Washington Ignacio on 27/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
